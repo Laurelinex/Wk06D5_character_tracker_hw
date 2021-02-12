6dd4ffec6301b6eed7ad26f990463f2a9a63970a
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-02-12 13:20:14 +0000

    Reset and Delete features added

--- hw_dnd_character_list/js/app.js
+++ hw_dnd_character_list/js/app.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const newCharacterForm = document.querySelector('#new-character-form');
     newCharacterForm.addEventListener('submit', handleNewCharacterFormSubmit);
+
+    const deleteAllButton = document.querySelector('#delete-all');
+    deleteAllButton.addEventListener('click', handleDeleteAllClick);
 });
 
 const handleNewCharacterFormSubmit = function (event) {
@@ -10,4 +13,11 @@ const handleNewCharacterFormSubmit = function (event) {
     characterListItem.textContent = `${this.name.value} the ${this.kin.value}: a ${this.alignment.value} ${this.class.value}`;
     const characterList = document.querySelector('#character-list');
     characterList.appendChild(characterListItem);
+
+    this.reset();
 };
+
+const handleDeleteAllClick = function() {
+    const characterList = document.querySelector('#character-list');
+    characterList.innerHTML = '';
+}
